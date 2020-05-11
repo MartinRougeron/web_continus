cf93451d36614d1aaed9cef917db6bbf174a7ea9
martin.rougeron@gmail.com <45298909+LOLISK@users.noreply.github.com>
2020-05-11 19:05:45 +0200

    feat : build

---
+++ build/precache-manifest.e3415629282853dbb258ce13bbc37e4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8f4fa597c55378ce5f6fa95cb4274e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da8364a3da3e0b6bb254",
+    "url": "/static/css/main.4f17c76b.chunk.css"
+  },
+  {
+    "revision": "c8338cf234fcbd14193a",
+    "url": "/static/js/2.78290df2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.78290df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8364a3da3e0b6bb254",
+    "url": "/static/js/main.29025e88.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  }
+]);
