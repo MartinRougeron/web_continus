f813940ede8ea83a48c5ba6db3738ecb43a3365a
martin <martin.rougeron@epitech.eu>
2020-05-14 10:25:04 +0200

    feat : test build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-rm -rf static
-rm -rf precache*
 cd ..
 yarn build
-cp -r build/* web_continus
+cp -r build/static/ web_continus/
+cp -r build/service-worker.js  web_continus/
+cp -r build/asset-manifest.json  web_continus/
+cp -r build/index.html  web_continus/
+cp -r build/robots.txt  web_continus/
+rm -f web_continus/precache-manifest.*
+cp -r build/precache-manifest.*  web_continus/
+cd web_continus/
