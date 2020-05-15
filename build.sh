044f903213fd9c99c7308cc4e1ceebd873b63dab
martin <martin.rougeron@epitech.eu>
2020-05-15 18:11:07 +0200

    fix: build.sh -> add copy public folder

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ cp -r build/static/ web_continus/
 cp -r build/service-worker.js  web_continus/
 cp -r build/asset-manifest.json  web_continus/
 cp -r build/index.html  web_continus/
+cp -r build/public  web_continus/
 cp -r build/robots.txt  web_continus/
 rm -f web_continus/precache-manifest.*
 cp -r build/precache-manifest.*  web_continus/
